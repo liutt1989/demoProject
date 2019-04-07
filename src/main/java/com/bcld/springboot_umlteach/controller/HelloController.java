07e0a62827ab710b2c9dc1f191d218c7133ab7f8
liutt1989 <liutingtingtorrie@163.com>
2019-04-07 12:53:56 +0800

    修改首页

--- src/main/java/com/bcld/springboot_umlteach/controller/HelloController.java
+++ src/main/java/com/bcld/springboot_umlteach/controller/HelloController.java
@@ -1,10 +1,14 @@
 package com.bcld.springboot_umlteach.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
 
-
+    @RequestMapping("/index")
+    public String getIndex(){
+        return "test";
+    }
 
 }
