b121d608042df68e8f882f0bd026da97b1052b9f
liutt1989 <liutingtingtorrie@163.com>
2019-04-07 12:19:22 +0800

    上传项目

---
+++ src/main/java/com/bcld/springboot_umlteach/SpringbootUmlteachApplication.java
@@ -0,0 +1,13 @@
+package com.bcld.springboot_umlteach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootUmlteachApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootUmlteachApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bcld/springboot_umlteach/SpringbootUmlteachApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bcld.springboot_umlteach;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootUmlteachApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
